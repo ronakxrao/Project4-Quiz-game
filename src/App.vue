be1e27582f5b072f34088ef3d0931fea3d635fa7
ronakxrao <yadavronak301@gmail.com>
2024-08-25 09:03:39 +0530

    Added new input

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,14 @@
     Microphones can be used not omly to pick up sound, but also to project sound similar to a speaker.
   </h1>
 
-  
+  <input type="radio" name="options" value="True">
+    <label>True</label><br>
+
+    <input type="radio" name="options" value="False">
+    <label>False</label><br>
+    
+    <button class="send" type="button">Send</button>
+
   </div>
 </template>
 
