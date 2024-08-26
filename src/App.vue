f86b2543546038baf1cddc3d5534be325befbfbb
ronakxrao <yadavronak301@gmail.com>
2024-08-26 20:35:03 +0530

    Project completed

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,116 @@
 <template>
 
   <div>
-    <h1 v-html="this.question">
-    </h1>
 
-    <input type="radio" name="options" value="True">
-    <label>True</label><br>
+    <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount" />
 
-    <input type="radio" name="options" value="False">
-    <label>False</label><br>
+    <template v-if="this.question">
+      <h1 v-html="this.question">
+      </h1>
 
-    <button class="send" type="button">Send</button>
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input 
+        :disabled="this.answerSumbitted"
+        type="radio" 
+        name="options" 
+        :value="answer" 
+        v-model="this.chosenAnswer"
+        >
+        <label v-html="answer"></label><br>
+      </template>
+
+      <button v-if="!this.answerSumbitted" @click="this.sumbitAnswer()" class="send" type="button">Send</button>
+
+      <section v-if="this.answerSumbitted" class="result">
+        <h4 v-if="this.chosenAnswer == this.correctAnswer"
+        v-html="'&#9989; Congratulations, the answer ' +  this.correctAnswer + ' is correct.'">
+          
+        </h4>
+
+        <h4 v-else
+        v-html="'&#10060; I`m sorry, you picked the wrong answer. The correct is ' + this.correctAnswer + '.'">
+
+        </h4>
+
+        <button @click="this.getNewQuestion()" class="send" type="button">Next question</button>
+      </section>
+    </template>
+
 
   </div>
 </template>
 
 <script>
 
+import ScoreBoard from '@/components/ScoreBoard.vue'
 
 export default {
+
   name: 'App',
+  components: {
+    ScoreBoard
+  },
 
   data() {
     return {
       question: undefined,
       incorrectAnswers: undefined,
-      correctAnswers: undefined
+      correctAnswer: undefined,
+      chosenAnswer: undefined,
+      answerSumbitted: false,
+      winCount: 0,
+      loseCount: 0
     }
   },
   computed: {
-   answers() {
-    var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
-    answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswers);
-    return answers;
-   }
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      return answers;
+    }
   },
-  created() {
-    this.axios
-      .get('https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple')
+  methods: {
+    sumbitAnswer() {
+      if (!this.chosenAnswer) {
+        alert('Pick one of the options');
+      } else{
+        this.answerSumbitted=true;
+        if(this.chosenAnswer == this.correctAnswer) {
+          this.winCount++;
+        } else {
+          this.loseCount++;
+        }
+      }
+    },
+
+    getNewQuestion() {
+
+     this.answerSumbitted=false;
+     this.chosenAnswer= undefined;
+     this.question = undefined;
+
+      this.axios
+      .get('https://opentdb.com/api.php?amount=1&difficulty=easy')
       .then((response) => {
         this.question = response.data.results[0].question;
         this.incorrectAnswers = response.data.results[0].incorrect_answers;
-        this.correctAnswers = response.data.results[0].correct_answer;
+        this.correctAnswer = response.data.results[0].correct_answer;
 
-      })
+  });
+    }
+
+  },
+
+  
+  
+ 
+  created() {
+    this.getNewQuestion();
   }
 
 }
 
-//https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple
+//https://opentdb.com/api.php?amount=1&difficulty=easy
 </script>
 
 <style lang="scss">
